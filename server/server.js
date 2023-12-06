ab03f3a5e2c29cdf1bd34679b9c3eab69bece29c
Anthony Barragan <anthony.bar.89@gmail.com>
2023-12-06 07:52:26 -0800

    setting alter and force to false in server file

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 
-db.sync({ alter: true, force: true })
+db.sync({ alter: false, force: false })
   .then(() => {
     console.log('Database connected successfully');
     // Enable Sequelize query logging
