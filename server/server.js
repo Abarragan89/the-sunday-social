8cb84979e72bc014e0e86e5776903a3554640ea0
Anthony Barragan <anthony.bar.89@gmail.com>
2024-01-13 08:36:24 -0800

    uncommented out socket connection to test heroku deployment

--- server/server.js
+++ server/server.js
@@ -20,36 +20,36 @@ const io = new Server(server, {
   }
 })
 
-// io.on('connection', (socket) => {
-//   // set a join room event. The front end will first join room 
-//   socket.on('join_room', (data) => {
-//     socket.join(data)
-//   })
-//   // set a leave room event
-//   socket.on('leave_room', (chatId) => {
-//     socket.leave(chatId)
-//   })
+io.on('connection', (socket) => {
+  // set a join room event. The front end will first join room 
+  socket.on('join_room', (data) => {
+    socket.join(data)
+  })
+  // set a leave room event
+  socket.on('leave_room', (chatId) => {
+    socket.leave(chatId)
+  })
 
-//   // the 'send_message' will be called at the end of the send message route 
-//   socket.on('send_message', (data) => {
-//     // the recieved_message event will have a call back function that will
-//     // call getMessages() to rerender the messages on the user side
-//     socket.to(data.chatId).emit('recieve_message', data)
-//   })
+  // the 'send_message' will be called at the end of the send message route 
+  socket.on('send_message', (data) => {
+    // the recieved_message event will have a call back function that will
+    // call getMessages() to rerender the messages on the user side
+    socket.to(data.chatId).emit('recieve_message', data)
+  })
 
-//   // Add cleanup code here for removing event listeners when no longer needed
-//   const cleanupListeners = () => {
-//     socket.off('join_room');
-//     socket.off('leave_room');
-//     socket.off('send_message');
-//   };
+  // Add cleanup code here for removing event listeners when no longer needed
+  const cleanupListeners = () => {
+    socket.off('join_room');
+    socket.off('leave_room');
+    socket.off('send_message');
+  };
 
-//   // Example: remove listeners on socket disconnect
-//   socket.on('disconnect', () => {
-//     cleanupListeners();
-//   });
+  // Example: remove listeners on socket disconnect
+  socket.on('disconnect', () => {
+    cleanupListeners();
+  });
 
-// })
+})
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
